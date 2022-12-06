a6d70312aee3e708b5448877653f8d7c8f826102
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-12-06 10:59:48 -0600

    Add github users table to db

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,10 +2,19 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
 
+DROP TABLE IF EXISTS github_users;
+
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email VARCHAR,
   password_hash VARCHAR NOT NULL,
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL
+);
+
+CREATE TABLE github_users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  login TEXT NOT NULL,
+  email TEXT,
+  avatar TEXT
 );
