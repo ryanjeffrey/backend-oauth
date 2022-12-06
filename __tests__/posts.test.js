a9d9f17e63f12f5c229ccd02332317bcc2e8800e
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-12-06 11:45:03 -0600

    Add test to post post

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -17,6 +17,25 @@ describe('post routes', () => {
     expect(res.status).toEqual(200);
   });
 
+  it('post route allows authenticated users to create posts linked to their id', async () => {
+    const testPost = {
+      title: 'Test Post',
+      content: 'This test post is limited to 255 characters.',
+    };
+
+    await agent.get('/api/v1/github/callback?code=42');
+    const res = await agent.post('/api/v1/posts').send(testPost);
+    expect(res.status).toBe(200);
+    expect(res.body).toMatchInlineSnapshot(`
+      Object {
+        "content": "This test post is limited to 255 characters.",
+        "id": "1",
+        "title": "Test Post",
+        "userId": "1",
+      }
+    `);
+  });
+
   afterAll(() => {
     pool.end();
   });
