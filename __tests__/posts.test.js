a629660de132c5d7818c067ef9afd00190d66ef3
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-12-07 16:58:15 -0600

    Capitalize service name

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-jest.mock('../lib/services/github');
+jest.mock('../lib/services/Github');
 const agent = request.agent(app);
 
 describe('post routes', () => {
