d3191a9e8c09a960251dc6bee24cdf10133dcf9f
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-12-08 13:50:04 -0600

    Lowercase github for services

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-jest.mock('../lib/services/Github');
+jest.mock('../lib/services/github');
 
 describe('github auth', () => {
   beforeEach(() => {
